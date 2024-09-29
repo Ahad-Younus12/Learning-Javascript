a171b0e3e640acb841e14818b32b6b4d6e7a2d51
Ahad-Younus12 <122182688+Ahad-Younus12@users.noreply.github.com>
2024-09-29 11:12:53 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =14455
+let accountemail = "ahad@gmail.com"
+var accountpassword ="122221"
+
+accountcity = "Ichikawa"
+
+let accountState;
+/* when we use semicolon, it means that value is not deined*/
+//prefer not to use var because of issue in block scope and functional scope //
+
+/* it is possible to save variable memory without writing anything before it but its not recommended */
+
+console.log(accountId);
+
+console.table([accountId, accountemail, accountpassword, accountcity, accountState]);
