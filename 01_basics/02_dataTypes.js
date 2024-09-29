4ffda79da08231c913ed55f3c9b50165e53a2f2d
Ahad-Younus12 <122182688+Ahad-Younus12@users.noreply.github.com>
2024-09-29 11:13:18 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version//
+
+// alert(3+3) // we are using nodejs, not browser cause alert is used in browser not nodejs
+
+console.log(3
+    +3) // writing like this is also possible but readability is very important when writing code
+
+console.log("Ahad");
+    
+
+let name = "Ahad"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
