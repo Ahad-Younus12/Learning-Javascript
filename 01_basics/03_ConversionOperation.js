1c6905f3096725e7031cc90069a612fb77c55fe4
Ahad-Younus12 <122182688+Ahad-Younus12@users.noreply.github.com>
2024-09-30 12:45:23 +0000

    Operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -31,5 +31,47 @@ let somenumber = 12
 
 let stringsomenumber = String(somenumber)
 
-console.log(stringsomenumber);
-console.log(typeof stringsomenumber);
+// console.log(stringsomenumber);
+// console.log(typeof stringsomenumber);
+
+
+//************************Operations************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(9/3);
+// console.log(2%3);           **********************(?????)
+
+let str1 = "hello"
+let str2 = " Ahad"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); ............ (if the string is in the beginning, then all the values after are treated as strings)
+// console.log(1 + 2 + "2"); ............ (Not preferred to write like this)
+
+// console.log( (3 + 4) * 5 % 3); .......... (use parenthesis)
+
+// console.log(+true);   ............. (Not sensible to write code like this)
+// console.log(+"");     .............  *same as line 65*
+
+let num1, num2, num3         // ....... *same as line 65*
+
+num1 = num2 = num3 = 2 + 2    // ....... *same as line 65*
+
+let gameCounter = 100
+++gameCounter;                    // Read increment documentation(++) in MDN
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
