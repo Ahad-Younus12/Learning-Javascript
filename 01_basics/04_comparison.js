be46e3d217ed471bceb0c1ad27078c082e5f85fe
Ahad-Younus12 <122182688+Ahad-Younus12@users.noreply.github.com>
2024-10-05 08:22:47 +0000

    Compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);   // writing these types of conversion codes will always confuse, and avoid writing like this cause clean code is most important 
+console.log(null == 0);  //...........same as above
+console.log(null >= 0);  //...........same as above
+
+console.log(undefined == 0);  //...........same as above
+console.log(undefined > 0);  //...........same as above
+console.log(undefined < 0);  //...........same as above
+
+// === (triple equals strictly check and make comparisons)
+
+console.log("2" === 2);
